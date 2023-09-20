e713f130f0e0f69927539721406c3a1b04c782d2
XPROALEX <XPROALEX@users.noreply.github.com>
2023-09-20 12:44:26 +0200

    Add files via upload

---
+++ Where.sql
@@ -0,0 +1,19 @@
+SELECT * FROM meal WHERE price < 15.0;
+
+SELECT * FROM ingredient WHERE vegetarian = true;
+
+SELECT * FROM meal WHERE calories >= 500;
+
+SELECT * FROM meal WHERE price  BETWEEN 15.0 AND 16.0;
+
+SELECT * FROM ingredient WHERE contains_lactose = false AND gluten_free = true;
+
+SELECT * FROM meal WHERE price < 15.0 OR calories > 600;
+
+SELECT * FROM ingredient WHERE vegetarian = true OR gluten_free= true;
+
+SELECT * FROM meal WHERE name = 'Pest Pasta';
+
+SELECT * FROM ingredient WHERE contains_lactose=true;
+
+SELECT * FROM meal WHERE price > 20.0 AND calories < 800;
